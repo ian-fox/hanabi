227096430fe8fabf7b96ba8f7f8689d6c881101a
Ian Fox <ianfox97@gmail.com>
2017-01-08 00:50:42 -0500

    Add move classes

--- server/classes.py
+++ server/classes.py
@@ -1,13 +1,3 @@
-from enum import Enum
-
-class Colour(Enum):
-    BLUE = 1
-    GREEN = 2
-    RED = 3
-    WHITE = 4
-    YELLOW = 5
-    RAINBOW = 6
-
 class Hand:
     def __init__(self, player):
         self.cards = []
---
+++ server/colour.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Colour(Enum):
+    BLUE = 1
+    GREEN = 2
+    RED = 3
+    WHITE = 4
+    YELLOW = 5
+    RAINBOW = 6
