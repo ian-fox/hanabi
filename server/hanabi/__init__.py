35f7ca4ae05fd4e4e10b597c4cbc02359b79139b
Ian Fox <ianfox97@gmail.com>
2017-01-21 13:01:31 -0500

    Commit on teardown

--- server/hanabi/__init__.py
+++ server/hanabi/__init__.py
@@ -8,6 +8,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     config[config_name].init_app(app)
 
     db.init_app(app)
