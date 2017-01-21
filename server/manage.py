d0a5aea44c15c4b199269cdd44c5ae406a3368f9
Ian Fox <ianfox97@gmail.com>
2017-01-21 14:10:05 -0500

    Add coverage to unit test runner

--- server/manage.py
+++ server/manage.py
@@ -3,6 +3,12 @@ import os
 from hanabi import create_app, db
 from flask_script import Manager, Shell
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='hanabi/*')
+    COV.start()
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
@@ -16,12 +22,26 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 if __name__ == '__main__':
     manager.run()
