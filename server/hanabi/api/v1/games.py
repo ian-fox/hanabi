3472b39b5c2997e9973515c8db152b3d1dba3e5f
Ian Fox <ianfox97@gmail.com>
2017-01-21 13:13:44 -0500

    Add default options for games

--- server/hanabi/api/v1/games.py
+++ server/hanabi/api/v1/games.py
@@ -1,7 +1,11 @@
-from flask import jsonify
-
 from . import api
+from hanabi import db
+from hanabi.classes import *
 
 @api.route('/games', methods=['GET'])
 def get_games():
-    return jsonify({'test': 'ing'})
+    return Game.query.filter_by(started=False).all()
+
+@api.route('/games/new', methods=['POST'])
+def new_game(opts):
+    return opts
--- server/hanabi/classes.py
+++ server/hanabi/classes.py
@@ -1,4 +1,5 @@
 from . import db
+from hanabi.colour import Colour
 
 class Hand:
     def __init__(self, player):
@@ -12,6 +13,10 @@ class Card:
         self.colourKnown = False
         self.rankKnown = False
 
+def newDeck():
+    """Return a full, shuffled deck in string form"""
+    return "a deck"
+
 class Player:
     def __init__(self, hand):
         self.hand = hand
@@ -20,18 +25,18 @@ class Game(db.Model):
     __tablename__ = 'games'
     id = db.Column(db.Integer, primary_key=True)
 
-    discard = db.Column(db.PickleType)
-    deck = db.Column(db.PickleType)
-    hands = db.Column(db.PickleType)
-    hints = db.Column(db.SmallInteger)
-    inPlay = db.Column(db.PickleType)
-    misfires = db.Column(db.SmallInteger)
-    perfectOrBust = db.Column(db.Boolean)
-    players = db.Column(db.PickleType)
-    public = db.Column(db.Boolean)
-    rainbowIsColour = db.Column(db.Boolean)
-    started = db.Column(db.Boolean, index=True)
-    turn = db.Column(db.SmallInteger)
+    discard = db.Column(db.PickleType, default=dict.fromkeys(list(Colour)))
+    deck = db.Column(db.PickleType, default=newDeck())
+    hands = db.Column(db.PickleType, default=[])
+    hints = db.Column(db.SmallInteger, default=8)
+    inPlay = db.Column(db.PickleType, default=dict.fromkeys(list(Colour)))
+    misfires = db.Column(db.SmallInteger, default=3)
+    perfectOrBust = db.Column(db.Boolean, default=False)
+    players = db.Column(db.PickleType, default=[])
+    public = db.Column(db.Boolean, default=False)
+    rainbowIsColour = db.Column(db.Boolean, default=False)
+    started = db.Column(db.Boolean, index=True, default=False)
+    turn = db.Column(db.SmallInteger, default=0)
 
     def __repr__(self):
         return '<Game %r>' % self.id
