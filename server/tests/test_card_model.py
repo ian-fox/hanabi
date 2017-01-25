ea4d30ad3f7ba845fd26d5703b942d5176d99184
Ian Fox <ianfox97@gmail.com>
2017-01-25 01:17:13 -0500

    Move new_deck test case to test_card_model

--- server/tests/test_card_model.py
+++ server/tests/test_card_model.py
@@ -1,6 +1,6 @@
 import unittest
 
-from hanabi.models import Card, Colour
+from hanabi.models import Card, Colour, new_deck
 
 
 class CardModelTestCase(unittest.TestCase):
@@ -10,11 +10,42 @@ class CardModelTestCase(unittest.TestCase):
 
         self.assertEqual(c.colour, Colour.BLUE)
         self.assertEqual(c.rank, 1)
-        self.assertFalse(c.colourKnown)
+        self.assertIsNone(c.colourKnown)
         self.assertFalse(c.rankKnown)
 
         c = Card(42)
         self.assertEqual(c.colour, Colour.YELLOW)
         self.assertEqual(c.rank, 2)
-        self.assertFalse(c.colourKnown)
+        self.assertIsNone(c.colourKnown)
         self.assertFalse(c.rankKnown)
+
+
+class NewDeckTestCase(unittest.TestCase):
+    def test_new_deck(self):
+        """Correctly generates a deck"""
+        d = new_deck()
+
+        self.assertEqual(len(d), 60)
+
+        # Three of each one
+        for colour in range(6):
+            self.assertEqual(len(list(filter(lambda x: x == 10 * colour + 1, d))), 3)
+
+        # One of each five
+        for colour in range(6):
+            self.assertIn(10 * colour + 5, d)
+
+        # Two of everything else
+        for colour in range(6):
+            for rank in range(2, 5):
+                self.assertEqual(len(list(filter(lambda x: x == 10 * colour + rank, d))), 2)
+
+    def test_new_hard_deck(self):
+        """Correctly generates a hard deck"""
+        d = new_deck(True)
+
+        self.assertEqual(len(d), 55)
+
+        # One of each rainbow
+        for rank in range(1, 5):
+            self.assertEqual(len(list(filter(lambda x: x == 50 + rank, d))), 1)
